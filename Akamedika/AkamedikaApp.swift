bc770964fc6a8f0cc272dabffbbc8384d5e0f24f
Abdüssamet Aslan <drabdussametaslan@gmail.com>
2026-04-12 08:26:24 +0300

    Initial Commit

---
+++ Akamedika/AkamedikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkamedikaApp.swift
+//  Akamedika
+//
+//  Created by Abdüssamet Aslan on 12.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AkamedikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
